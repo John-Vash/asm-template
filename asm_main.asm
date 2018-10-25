428bbc985c4097356d8223f0e4cfb405d1542d3c
John-Vash <john.vas.jr@gmail.com>
2018-10-25 12:49:08 -0700

    initial push

---
+++ asm_main.asm
@@ -0,0 +1,38 @@
+;
+; file: asm_main.asm
+
+%include "asm_io.inc"
+;
+; initialized data is put in the .data segment
+;
+segment .data
+        syswrite: equ 4
+        stdout: equ 1
+        exit: equ 1
+        SUCCESS: equ 0
+        kernelcall: equ 80h
+
+
+
+; uninitialized data is put in the .bss segment
+;
+segment .bss
+
+;
+; code is put in the .text segment
+;
+segment .text
+        global  asm_main
+asm_main:
+        enter   0,0               ; setup routine
+        pusha
+; *********** Start  Assignment Code *******************
+
+; *********** End Assignment Code **********************
+
+        popa
+        mov     eax, SUCCESS       ; return back to the C program
+        leave                     
+        ret
+
+
